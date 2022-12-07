1317ecd6adfbc2e12aaefab0aff4017d301bf49e
EmeDeMarcelo <marcelojara23@gmail.com>
2022-12-07 08:38:00 -0300

    first commit

---
+++ NetCoreYouTube/Controllers/ProductoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreYouTube.Models;
+using NetCoreYouTube.Recursos;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace NetCoreYouTube.Controllers
+{
+    [ApiController]
+    [Route("Producto")]
+    public class ProductoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Listar")]
+        public dynamic ListarProductos(bool estado)
+        {
+            List<Parametro> parametros = new List<Parametro>
+            {
+                new Parametro("@Estado", "1")
+            };
+            DataTable tCategoria = DBDatos.Listar("Categoria_Listar", parametros);
+            DataTable tProducto = DBDatos.Listar("Producto_Listar");
+
+            string JsonCategoria = JsonConvert.SerializeObject(tCategoria);
+            string JsonProducto = JsonConvert.SerializeObject(tProducto);
+            return new
+            {
+                success = true,
+                message = "exito",
+                result = new
+                {
+                    categoria = JsonConvert.DeserializeObject<List<Categoria>>(JsonCategoria),
+                    producto = JsonConvert.DeserializeObject<List<Producto>>(JsonProducto)
+                }
+            };
+        }
+    }
+}
